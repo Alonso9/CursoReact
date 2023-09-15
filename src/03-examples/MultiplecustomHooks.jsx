965e04dce0fdf018b38bd9292cbc53885ad6de7b
Alonso9 <60718869+Alonso9@users.noreply.github.com>
2023-09-15 14:07:27 -0600

    Clase 129

--- src/03-examples/MultiplecustomHooks.jsx
+++ src/03-examples/MultiplecustomHooks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useFetch } from '../hooks/useFetch';
+// import { useFetch } from '../hooks/useFetch';
+// import { useCounter } from '../hooks/useCounter';
+import { useFetch, useCounter } from '../hooks';
 import './css/style.css';
 
 export const MultiplecustomHooks = () => {
-  const { data, isLoading, haserror } = useFetch(
-    'https://api.breakingbadquotes.xyz/v1/quotes/5'
-  );
+  const { increment, counter } = useCounter(1);
+  const url = 'https://api.breakingbadquotes.xyz/v1/quotes/' + counter;
+  const { data, isLoading, haserror } = useFetch(url);
 
   console.log({ data, isLoading, haserror });
 
@@ -26,6 +28,16 @@ export const MultiplecustomHooks = () => {
           <footer className="blockquote-footer">{data[0].author}</footer>
         </blockquote>
       )}
+
+      <button
+        className="btn btn-primary"
+        onClick={() => increment(1)}
+        disabled={isLoading}
+      >
+        Next quote
+      </button>
+      <h1>{counter}</h1>
+      <h1>{url}</h1>
     </>
   );
 };
---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+// Creamos este archivo de barril para exportar todos los Hooks de esta carpeta y ya solo detructurar en donde lo ocupemos
+
+export * from './useCounter';
+export * from './useFetch';
+export * from './useForm';
